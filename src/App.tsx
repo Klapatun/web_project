e5696d6414ad5b6021239924e4fbbc27b80906b2
Klapatun Anton <klapatun@skbkontur.ru>
2023-02-24 22:42:03 +0700

    added lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { Counter } from "./components/Counter/Counter";
 import "./index.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Link } from "react-router-dom";
+import { Suspense } from "react";
 
 export const App = () => {
   return (
-    <div className="app">
-      dvbwukb
-      <Counter />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Link to="/">Главная</Link>
+        <Link to="/about">О сайте</Link>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/about" element={<AboutPageAsync />} />
+            <Route path="/" element={<MainPageAsync />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </BrowserRouter>
   );
 };
