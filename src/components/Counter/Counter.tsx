1b69a9400e43b28b64e36a3633600d0d2c05113c
Klapatun Anton <klapatun@skbkontur.ru>
2023-02-24 21:33:31 +0700

    added App component + changed css-loader

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
-import './Counter.scss'
-
+import classes from "./Counter.module.scss";
 
 export const Counter = () => {
-    const [count, setCount] = useState<number>(0);
+  const [count, setCount] = useState<number>(0);
 
-    const incriment = () => {
-        setCount(count+1);
-    }
+  const incriment = () => {
+    setCount(count + 1);
+  };
 
-    return (
-        <div>
-            <h1>{count}</h1>
-            <button onClick={incriment}>incriment</button>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={incriment} className={classes.btn}>
+        incriment
+      </button>
+    </div>
+  );
+};
