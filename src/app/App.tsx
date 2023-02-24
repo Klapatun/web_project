cd50a4c494f08c45dfc9b54ad9851876822c470a
Klapatun Anton <klapatun@skbkontur.ru>
2023-02-25 02:05:30 +0700

    moved files in pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 import { Suspense } from "react";
 import { classNames } from "helpers/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProviders/lib/useTheme";
-import { AboutPageAsync } from "pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "pages/MainPage/MainPage.async";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -19,8 +19,8 @@ export const App = () => {
       <Link to="/about">О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path="/about" element={<AboutPageAsync />} />
-          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/" element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
