71e4f34c0e00b08ccdb3cc50b4c30297e664eb58
Klapatun <klapatuna@mail.ru>
2023-02-25 14:34:29 +0700

    added router config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,8 @@
 import "./styles/index.scss";
-import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { Suspense } from "react";
 import { useTheme } from "app/providers/ThemeProviders/lib/useTheme";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
 import { classNames } from "shared/lib/classNames/classNames";
+import { AppRouter } from "./providers/router";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -17,12 +14,7 @@ export const App = () => {
       </div>
       <Link to="/">Главная</Link>
       <Link to="/about">О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/" element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
