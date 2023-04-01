217393723146f693528955e55ac585532398e244
Klapatun Anton <klapatun@skbkontur.ru>
2023-04-02 03:22:58 +0700

    added svg and file loader + replaced button to icon

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,20 +1,22 @@
 import cls from "./ThemeSwitchr.module.scss";
-import { classNames } from "shared/lib/classNames/classNames";
-import { useTheme } from "app/providers/ThemeProviders";
+import {classNames} from "../../../shared/lib/classNames/classNames";
+import {Theme, useTheme} from "../../../app/providers/ThemeProviders";
+import LightTheme from '../../../shared/assets/icons/theme-light.svg'
+import DarkTheme from '../../../shared/assets/icons/theme-dark.svg'
 
 interface ThemeSwitchrProps {
   className?: string;
 }
 
 export const ThemeSwitchr = ({ className }: ThemeSwitchrProps) => {
-  const { toggleTheme } = useTheme();
+  const { toggleTheme, theme } = useTheme();
 
   return (
     <button
       className={classNames(cls.themeSwitcher, {}, [className])}
       onClick={toggleTheme}
     >
-      Поменять тему
+      {theme === Theme.LIGHT ? <LightTheme /> : <DarkTheme />}
     </button>
   );
 };
