edc2004ae1c172adaa7803602f8cd691c8f7fc72
Klapatun Anton <klapatun@skbkontur.ru>
2023-04-02 17:05:31 +0700

    added Button component

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,8 +1,9 @@
 import cls from "./ThemeSwitchr.module.scss";
-import {classNames} from "../../../shared/lib/classNames/classNames";
-import {Theme, useTheme} from "../../../app/providers/ThemeProviders";
+import {classNames} from "shared/lib/classNames/classNames";
+import {Theme, useTheme} from "app/providers/ThemeProviders";
 import LightTheme from '../../../shared/assets/icons/theme-light.svg'
 import DarkTheme from '../../../shared/assets/icons/theme-dark.svg'
+import {Button, ThemeButton} from "shared/ui/Button/Button";
 
 interface ThemeSwitchrProps {
   className?: string;
@@ -12,11 +13,12 @@ export const ThemeSwitchr = ({ className }: ThemeSwitchrProps) => {
   const { toggleTheme, theme } = useTheme();
 
   return (
-    <button
-      className={classNames(cls.themeSwitcher, {}, [className])}
-      onClick={toggleTheme}
+    <Button
+        theme={ThemeButton.CLEAR}
+        className={classNames(cls.themeSwitcher, {}, [className])}
+        onClick={toggleTheme}
     >
       {theme === Theme.LIGHT ? <LightTheme /> : <DarkTheme />}
-    </button>
+    </Button>
   );
 };
