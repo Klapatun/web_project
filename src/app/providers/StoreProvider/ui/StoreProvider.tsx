da42e401453439a377a9c17330736a036d55ef5b
Klapatun Anton <klapatun@skbkontur.ru>
2024-03-17 08:30:07 +0700

    add test for counter

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -2,13 +2,14 @@ import {FC} from 'react';
 import { Provider } from 'react-redux';
 import {createReduxStore} from 'app/providers/StoreProvider';
 import {StateSchema} from 'app/providers/StoreProvider/config/StateSchema';
+import {DeepPartial} from '@reduxjs/toolkit';
 
 interface Props {
-	initialState?: StateSchema;
+	initialState?: DeepPartial<StateSchema>;
 }
 export const StoreProvider: FC<Props> = ({children, initialState}) => {
 
-	const store = createReduxStore(initialState);
+	const store = createReduxStore(initialState as StateSchema);
 
 	return (
 		<Provider store={store}>
--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -2,11 +2,14 @@ import {Button} from 'shared/ui/Button/Button';
 import {useDispatch, useSelector} from 'react-redux';
 import {counterActions} from '../modal/slice/counterSlice';
 import { getCounterValue } from '../modal/selectors/getCounterValue/getCounterValue';
+import {useTranslation} from 'react-i18next';
 
 export const Counter = () => {
 
 	const dispatch = useDispatch();
 	const counterValue = useSelector(getCounterValue);
+
+	const {t} = useTranslation();
 
 	const increment = () => {
 		dispatch(counterActions.increment());
@@ -18,9 +21,9 @@ export const Counter = () => {
 
 	return (
 		<div>
-			<h1>value = {counterValue}</h1>
-			<Button onClick={increment}>increment</Button>
-			<Button onClick={decrement}>decrement</Button>
+			<h1 data-testid='value-title'>value = {t('counterValue')}</h1>
+			<Button data-testid='increment-btn' onClick={increment}>{t('increment')}</Button>
+			<Button data-testid='decrement-btn' onClick={decrement}>{t('decrement')}</Button>
 		</div>
 	);
 };
